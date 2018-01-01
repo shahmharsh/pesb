b0fdf5f01aedd7c9fab235f53558882f8e8769e1
Harsh <shahmharsh@gmail.com>
2018-01-01 10:46:26 -0800

    table view dummy data

--- pesb/ViewController.m
+++ pesb/ViewController.m
@@ -8,22 +8,40 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+static NSString *kSimpleTableIdentifier = @"SimpleTableItem";
 
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (nonatomic, strong) NSArray *data;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.data = [NSArray arrayWithObjects:@"1", @"2", @"3", @"4", @"5", nil];
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:kSimpleTableIdentifier];
 }
 
+#pragma mark Table view delegate methods
+- (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:kSimpleTableIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:kSimpleTableIdentifier];
+    }
+    
+    cell.textLabel.text = [self.data objectAtIndex:indexPath.row];
+    return cell;
+}
 
+- (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.data.count;
+}
 @end
